d1ebc8fa99da5b414b1342750e03df4451c4ff8b
ZheKaii <s10258524@connect.np.edu.sg>
2024-05-21 14:40:29 +0800

    completed week 6

--- Week_4/books-api-mvc-db/app.js
+++ Week_4/books-api-mvc-db/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const booksController = require("./controllers/booksController");
+const usersController = require("./controllers/usersController");
 const sql = require("mssql");
 const dbConfig = require("./dbConfig");
 const bodyParser = require("body-parser"); // Import body-parser
@@ -22,6 +23,15 @@ app.get("/books/:id", booksController.getBookById);
 app.post("/books", validateBook, booksController.createBook); // POST for creating books (can handle JSON data)
 app.put("/books/:id", validateBook, booksController.updateBook); // PUT for updating books
 app.delete("/books/:id", booksController.deleteBook); // DELETE for deleting books
+
+app.get("/users/search", usersController.searchUsers);
+app.get("/users/with-books", usersController.getUsersWithBooks);
+
+app.post("/users", usersController.createUser); // Create user
+app.get("/users", usersController.getAllUsers); // Get all users
+app.get("/users/:id", usersController.getUserById); // Get user by ID
+app.put("/users/:id", usersController.updateUser); // Update user
+app.delete("/users/:id", usersController.deleteUser); // Delete user
 
 app.listen(port, async () => {
   try {
